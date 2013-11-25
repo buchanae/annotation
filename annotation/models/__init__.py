a60a9a1b5ea9e2c7ccafdf319c3660f92f5eca52
Alex Buchanan <buchanae@gmail.com>
2013-11-25 15:57:01 -0800

    remove unused TreeNode

--- annotation/models/__init__.py
+++ annotation/models/__init__.py
@@ -8,36 +8,6 @@ __version__ = '2.0.0'
 # TODO drop strand and use a boolean "reversed" or "reverse_strand" instead?
 
 
-class TreeNode(object):
-    '''Dead simple representation of a tree node.
-
-    A tree node has one parent and multiple children.
-
-    When a TreeNode's parent is set, the child is added to the parent's
-    children automatically, and likewise if the parent is unset,
-    the child is removed from the parent's children.
-
-    Note, this does not (yet) handle the opposite, i.e. if you remove
-    a child node from the parent, that child's parent is not unset.
-    '''
-    def __init__(self):
-        self._parent = None
-        self.children = []
-
-    @property
-    def parent(self):
-        return self._parent
-
-    @parent.setter
-    def parent(self, value):
-        # If this node already has a parent,
-        # delete this node from that parent's children
-        if self._parent:
-            self._parent.children.remove(self)
-        self._parent = value
-        self._parent.children.append(self)
-
-
 class Parent(object):
     def __init__(self, Parent_cls, name=None, related_name=None):
         self._name = name
