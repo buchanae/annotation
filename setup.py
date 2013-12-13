cb7c070d27896ee47e5d2a70daf24cc09dfe3fd3
Alex Buchanan <buchanae@gmail.com>
2013-12-13 13:32:22 -0800

    update setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ import os
 
 _this_dir = os.path.dirname(__file__)
 README_path = os.path.join(_this_dir, 'README.md')
+README = open(README_path).read()
 
 setup(
     name='annotation',
-    description='TODO',
-    long_description=open(README_path).read(),
-    version='1.0.0',
+    description='Tools for working with genome annotations.',
+    long_description=README,
+    version='2.0.0',
     author='Alex Buchanan',
     author_email='buchanae@gmail.com',
-    license='Apache',
+    license='MIT',
     py_modules=[
-        'annotation',
         'annotation.models',
         'annotation.builders',
         'annotation.builders.core',
