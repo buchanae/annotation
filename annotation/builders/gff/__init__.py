2d13c214d1af15154653bb86299176208ffe6f05
Alex Buchanan <buchanae@gmail.com>
2014-01-31 13:22:30 -0800

    reorganization

---
+++ annotation/builders/gff/__init__.py
@@ -0,0 +1,49 @@
+# TODO handle multipe parents when linking
+from __future__ import absolute_import
+
+from annotation import models
+from annotation.builders.core import Builder
+from annotation.builders.gff import handlers
+
+
+class ReaderBase(object):
+
+    Builder = Builder
+
+    def read(self, records):
+        builder = self.Builder()
+
+        for handler in self.handlers:
+            handler(builder, self.Models)
+
+        references = []
+        def collect_references(node, record):
+            if isinstance(node, self.Models.Reference):
+                references.append(node)
+
+        builder.post_transform.append(collect_references)
+
+        builder.build(records)
+        return references
+
+
+class Reader(ReaderBase):
+
+    handlers = [
+        handlers.ReferenceHandler,
+        handlers.GeneHandler,
+        handlers.TranscriptHandler,
+        handlers.ExonHandler,
+        handlers.CodingSequenceHandler,
+    ]
+
+    class Models:
+        Reference = models.Reference
+        Gene = models.Gene
+        Transcript = models.Transcript
+        Exon = models.Exon
+        CodingSequence = models.CodingSequence
+
+
+_default_reader = Reader()
+read = _default_reader.read
---
+++ annotation/builders/gff/core.py
@@ -0,0 +1,57 @@
+from annotation.builders.linker import Linker as LinkerBase
+
+
+class Linker(LinkerBase):
+
+    def __init__(self, parent_type, child_type, parent_attr,
+                 parent_ID_func=None):
+
+        super(Linker, self).__init__()
+        self.parent_type = parent_type
+        self.child_type = child_type
+        self.parent_attr = parent_attr
+        self.parent_ID_func = parent_ID_func
+
+    def _get_ID(self, node, record):
+        return node.ID
+
+    def _get_parent_ID(self, node, record):
+        if self.parent_ID_func:
+            return self.parent_ID_func(node, record)
+        else:
+            return record.parent_ID
+
+    def _link(self, child, parent):
+        setattr(child, self.parent_attr, parent)
+
+    def _index_parent(self, node, record):
+        if isinstance(node, self.parent_type):
+            super(Linker, self)._index_parent(node, record)
+
+    def _try_link(self, node, record):
+        if isinstance(node, self.child_type):
+            super(Linker, self)._try_link(node, record)
+
+
+class Handler(object):
+
+    Linker = Linker
+
+    def add_decoder(self, decode_fn, types):
+        def fn(record):
+            if record.type in types:
+                return decode_fn(record)
+        self.builder.transforms.append(fn)
+
+    def add_linker(self, *args, **kwargs):
+        linker = self.Linker(*args, **kwargs)
+        self.builder.add_handler(linker)
+
+    def __init__(self, builder, models):
+        self.builder = builder
+        try:
+            decode_fn = getattr(self, 'decode')
+            types = getattr(self, 'types')
+            self.add_decoder(decode_fn, types)
+        except AttributeError:
+            pass
